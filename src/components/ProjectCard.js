e65b7d039f0217a9df61817dc8877ae6ab8b6469
Manuel Gil <manuelgilmoltrasio@gmail.com>
2022-06-01 12:38:11 -0300

    added active state to gh button

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -8,9 +8,7 @@ function ProjectCard({ imgSrc, cardTitle, githubRepo, liveSite }) {
             <div className='divider'></div>
             <div className='project-info'>
                 <a href={liveSite}><h3 className='project-title'>{cardTitle}</h3></a>
-                <div className='icons-container'>
-                    <a href={githubRepo}><BsGithub id='gh' color='white'/></a>
-                </div>
+                <a href={githubRepo} id="icon-btn"><BsGithub id='gh' color='white'/></a>
             </div>
         </div>
     );
