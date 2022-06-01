af07236041299d514d9d482b67acaff4a3f3dfab
Manuel Gil <manuelgilmoltrasio@gmail.com>
2022-06-01 12:25:16 -0300

    changed card styles. Added live site and gh links

--- src/App.js
+++ src/App.js
@@ -6,18 +6,18 @@ function App() {
     return (
         <main className="App">
             <Navbar/>
-                <div className='home-section'>
-                    <h1>Hey Im Manuel</h1>
-                    <h2>a frontend developer</h2>
+            <div className='home-section' id='home-section'>
+                <h1>Hey Im Manuel</h1>
+                <h2>a frontend developer</h2>
+            </div>
+            <div className='work-section' id='work-section'>
+                <p className='projects-header'>These are some of my projects</p>
+                <div className='projects-container'>
+                    <ProjectCard imgSrc={require('./images/calculator.png')} cardTitle="Calculator" githubRepo="https://github.com/ManuGil22/calculator" liveSite="https://manugil22.github.io/calculator/" />
+                    <ProjectCard imgSrc={require('./images/todo-list.png')} cardTitle="To-do list" githubRepo="https://github.com/ManuGil22/to-do-list" liveSite="https://manugil22.github.io/to-do-list/" />
+                    <ProjectCard imgSrc={require('./images/advice-generator.png')} cardTitle="Advice generator" githubRepo="https://github.com/ManuGil22/advice-generator-app" liveSite="https://manugil22.github.io/advice-generator-app/" />
                 </div>
-                <div className='work-section'>
-                    <p className='projects-header'>These are some of my projects</p>
-                    <div className='projects-container'>
-                        <ProjectCard imgSrc={require('./images/todo-list.png')} cardTitle="To-do list"/>
-                        <ProjectCard imgSrc={require('./images/calculator.png')} cardTitle="Calculator"/>
-                        <ProjectCard imgSrc={require('./images/advice-generator.png')} cardTitle="Advice generator"/>
-                    </div>
-                </div>
+            </div>
         </main>
   );
 }
--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,11 +1,17 @@
 import '../stylesheets/ProjectCard.css'
+import { BsGithub } from 'react-icons/bs'
 
-function ProjectCard({ imgSrc, cardTitle }) {
+function ProjectCard({ imgSrc, cardTitle, githubRepo, liveSite }) {
     return(
         <div className='project-card'>
             <img className='card-img' src={imgSrc} alt={`${cardTitle} project screenshot`} />
             <div className='divider'></div>
-            <h3 className='project-title'>{cardTitle}</h3>
+            <div className='project-info'>
+                <a href={liveSite}><h3 className='project-title'>{cardTitle}</h3></a>
+                <div className='icons-container'>
+                    <a href={githubRepo}><BsGithub id='gh' color='white'/></a>
+                </div>
+            </div>
         </div>
     );
 };
